b65493f41afae01ddcb85ebc02c77dccd90a5720
Kuzmin Dmitriy <dk@duotek.ru>
2019-05-21 18:29:22 +0300

    add todo list, start some refactoring

--- app/src/main/java/ru/qqdasdzxc/supremebot/ui/main/fragment/MainFragment.kt
+++ app/src/main/java/ru/qqdasdzxc/supremebot/ui/main/fragment/MainFragment.kt
@@ -26,6 +26,7 @@ class MainFragment : BaseFragment<FragmentMainViewBinding>() {
         super.onViewCreated(view, savedInstanceState)
 
         initView()
+        loadStartPage()
     }
 
     private fun initView() {
@@ -43,91 +44,86 @@ class MainFragment : BaseFragment<FragmentMainViewBinding>() {
             }
 
             @SuppressLint("RestrictedApi")
-            override fun onPageFinished(view: WebView?, url: String?) {
+            override fun onPageFinished(view: WebView, url: String) {
                 super.onPageFinished(view, url)
 
-                url?.let { urlPage ->
-                    if (clothHref != null && urlPage.endsWith(clothHref!!)) {
-                        when (currentState) {
-                            OrderState.SINGLE_ITEM_STATE -> {
-                                val js = "javascript:(function(){document.getElementsByClassName('button')[2].click();})()"
+                if (clothHref != null && url.endsWith(clothHref!!)) {
+                    when (currentState) {
+                        OrderState.SINGLE_ITEM_STATE -> {
+                            val js = "javascript:(function(){document.getElementsByClassName('button')[2].click();})()"
 
-                                binding.mainWebView.evaluateJavascript(js) {
-                                    currentState = OrderState.ITEM_IN_BASKET_STATE
+                            binding.mainWebView.evaluateJavascript(js) {
+                                currentState = OrderState.ITEM_IN_BASKET_STATE
 
-                                    CoroutineScope(Dispatchers.IO).launch {
-                                        val doc = Jsoup.connect(urlPage).get()
-                                        doc.body()
-                                    }
-
-                                    val js1 = "javascript:(function(){document.getElementsByClassName('button')[1].click();})()"
-
-                                    binding.mainWebView.evaluateJavascript(js1) {
-                                        currentState = OrderState.CHECKOUT_STATE
-                                    }
-
+                                CoroutineScope(Dispatchers.IO).launch {
+                                    val doc = Jsoup.connect(url).get()
+                                    doc.body()
                                 }
-                            }
-                            OrderState.ITEM_IN_BASKET_STATE -> {
 
-                            }
-                            OrderState.CHECKOUT_STATE -> {
+                                val js1 = "javascript:(function(){document.getElementsByClassName('button')[1].click();})()"
 
-                            }
-                            else -> {
+                                binding.mainWebView.evaluateJavascript(js1) {
+                                    currentState = OrderState.CHECKOUT_STATE
+                                }
 
                             }
                         }
+                        OrderState.ITEM_IN_BASKET_STATE -> {
 
-                        return
-                    }
-
-                    if (urlPage.endsWith("checkout")) {
-
-
-//                        binding.mainWebView.loadUrl("javascript:HTMLOUT.processHTML(document.documentElement.outerHTML);")
-
-                        //visa american_express master solo paypal
-
-                        val js = "javascript:(function(){" +
-                                "document.getElementById('order_billing_name').value = 'asd';" +
-                                "document.getElementById('credit_card_type').value = 'master';" +
-                                "document.getElementById('order_tel').value = '+79374102309';" +
-                                "document.getElementById('order_terms').checked = 'true';" +
-                                "document.getElementsByClassName('button')[0].click();})()"
-
-
-                        binding.mainWebView.evaluateJavascript(js) {
-                            binding.mainWebView.loadUrl("javascript:HTMLOUT.processHTML(document.documentElement.outerHTML);")
                         }
+                        OrderState.CHECKOUT_STATE -> {
 
+                        }
+                        else -> {
+
+                        }
                     }
+
+                    return
+                }
+
+                if (url.endsWith("checkout")) {
+
+
+
+                    val js = "javascript:(function(){" +
+                            "document.getElementById('order_billing_name').value = 'asd';" +
+                            "document.getElementById('credit_card_type').value = 'master';" +
+                            "document.getElementById('order_tel').value = '+79374102309';" +
+                            "document.getElementById('order_terms').checked = 'true';" +
+                            "document.getElementsByClassName('button')[0].click();})()"
+
+
+                    binding.mainWebView.evaluateJavascript(js) {
+                        //binding.mainWebView.loadUrl("javascript:HTMLOUT.processHTML(document.documentElement.outerHTML);")
+                    }
+
                 }
 
 
                 //todo save state depends on finished url
             }
         }
-        binding.mainWebView.webChromeClient = object : WebChromeClient() {
+    }
 
-        }
-
-        binding.mainWebView.loadUrl("https://www.supremenewyork.com/shop/all/jackets")
-
-
+    private fun loadStartPage() {
+        //TODO show loading somehow
         CoroutineScope(Dispatchers.IO).launch {
+            //TODO load start page(1)
             val doc = Jsoup.connect("https://www.supremenewyork.com/shop/all/jackets").get()
-            doc.body()
 
             //todo подумать что лучше - цепать сразу все элементы или пройтись по чайлдам вручную
             //doc.child(0).child(1).child(2).child(1)
             //doc.getElementsByAttributeValueContaining("class", "turbolink_scroller")
             //doc.getElementsContainingOwnText("Apple Coaches")
-            val scroller = doc.allElements.firstOrNull { it.attributes().get("class") == "turbolink_scroller" }
+            //doc.allElements.firstOrNull { it.attributes().get("class") == "turbolink_scroller" }
+            val scroller = doc.child(0).child(1).child(2).child(1)
             val filteredChildren = scroller?.children()?.filter { element ->
                 val elementString = element.toString()
+                //todo check name(2) and color(3)
                 elementString.contains("Apple") && !elementString.contains("sold out")
             }
+
             filteredChildren?.let {
                 //беру первую шмотку рандомного цвета
                 clothHref = filteredChildren[0].child(0).child(0).attr("href")
@@ -136,18 +132,14 @@ class MainFragment : BaseFragment<FragmentMainViewBinding>() {
                 }
 
                 //берем элементы страницы конкретной шмотки
-                val doc1 = Jsoup.connect("https://www.supremenewyork.com$clothHref").get()
-                doc1.body()
-                val addHref = doc1.getElementById("cctrl").child(0).attr("action")
+//                val doc1 = Jsoup.connect("https://www.supremenewyork.com$clothHref").get()
+//                doc1.body()
+//                val addHref = doc1.getElementById("cctrl").child(0).attr("action")
                 //Large - 56390
             }
 
-            //сделать несколько модов
-            //1)взять конкретный цвет - и выключиться если цвет солдаут
-            //2)взять конкретный цвет - и взять первый(или рандом) цвет такой же шмотки если не солдаут
-            //3)взять рандомный цвет
-        }
 
+        }
     }
 }
 
