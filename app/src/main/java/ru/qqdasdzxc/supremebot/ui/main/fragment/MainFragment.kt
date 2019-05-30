222e2995cbdc2b27afa21befabc758cc7ec81e30
Kuzmin Dmitriy <dk@duotek.ru>
2019-05-30 17:17:51 +0300

    update app id

--- app/src/main/java/ru/qqdasdzxc/supremebot/ui/main/fragment/MainFragment.kt
+++ app/src/main/java/ru/qqdasdzxc/supremebot/ui/main/fragment/MainFragment.kt
@@ -46,9 +46,7 @@ class MainFragment : BaseFragment<FragmentMainViewBinding>(), HandleBackPressFra
     }
 
     private fun setWaitingUIState() {
-        binding.mainLogoView.show()
         binding.mainHelloLabelView.show()
-        binding.mainNickLabelView.show()
         binding.startButton.show()
         binding.testButton.show()
         binding.mainWebView.hide()
@@ -56,9 +54,7 @@ class MainFragment : BaseFragment<FragmentMainViewBinding>(), HandleBackPressFra
     }
 
     private fun setWorkingUIState() {
-        binding.mainLogoView.hide()
         binding.mainHelloLabelView.hide()
-        binding.mainNickLabelView.hide()
         binding.startButton.hide()
         binding.testButton.hide()
         binding.stopButton.show()
